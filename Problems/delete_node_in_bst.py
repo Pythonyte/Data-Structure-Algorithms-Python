58fed379aa580633791f4752bd47860f98b716fe
Sumit Singhal <sumitsinghal4390@gmail.com>
2019-09-21 14:00:33 +0530

    read me

--- Problems/delete_node_in_bst.py
+++ Problems/delete_node_in_bst.py
@@ -20,6 +20,8 @@ def delete_node_bst(current_node, key):
         # Now 3 options are there:
             1. if current_node is leaf simply maake it none
             2. If current_node has only one child, get than child value to current_node and remove child
+            3. If current node has both child, find inorder_successor_node, place value of it into current_node
+            and delete inorder_successor_node form current node right sub tree
         """
 
         if current_node.is_leaf():
